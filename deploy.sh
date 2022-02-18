298248dc555dc8ae98250939cfad82cba27571d9
parislyons <parislyons@hotmail.co.uk>
2022-02-18 14:18:50 +0000

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,8 +3,10 @@
 if [ ! -f data.db ]
   then ./install.sh
   echo "Successfully installed packages"
+  source .venv/bin/activate
   python3 create.py
   echo "Successfully created database"
 fi
 
+source .venv/bin/activate
 python3 app.py
